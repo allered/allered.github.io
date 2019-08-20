7edd9e4fd18a9858bd19db2f5e9833d43e370700
allered <alleredph@gmail.com>
2019-08-20 23:08:01 +0200

    3.0

--- js/backtotop.js
+++ js/backtotop.js
@@ -1,16 +1,17 @@
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+class Backtotop {
+  constructor(element) {
+    element.addEventListener('click', () => {
+      this.scrollIt(element);
+    });
+  }
 
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    document.getElementById("allered-backtotop__button").style.display = "block";
-  } else {
-    document.getElementById("allered-backtotop__button").style.display = "none";
+  scrollIt(element) {
+    window.scrollTo({
+      'behavior': 'smooth',
+      'left': 0,
+      'top': 0
+    });
   }
 }
 
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
+new Backtotop(document.querySelector('.allered-backtotop'));
