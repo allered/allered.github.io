5877c8af7029e39b1e0c42a80a29dba9e79ce116
allered <alleredph@gmail.com>
2019-08-21 00:19:36 +0200

    3.1

---
+++ js/typewriter.js
@@ -0,0 +1,38 @@
+// set up text to print, each item in array is new line
+var aText = new Array(
+    "My name is Alessandro Rossi, I'm a photographer and web developer from Cesenatico, Italy.",
+    "Feel free to contact me if you have any questions or business inquiries by filling the form below.");
+    var iSpeed = 100; // time delay of print out
+    var iIndex = 0; // start printing array at this posision
+    var iArrLength = aText[0].length; // the length of the text array
+    var iScrollAt = 20; // start scrolling up at this many lines
+     
+    var iTextPos = 0; // initialise text position
+    var sContents = ''; // initialise contents variable
+    var iRow; // initialise current row
+     
+    function typewriter()
+    {
+     sContents =  ' ';
+     iRow = Math.max(0, iIndex-iScrollAt);
+     var destination = document.querySelector('.allered-typewriter');
+     
+     while ( iRow < iIndex ) {
+      sContents += aText[iRow++] + '<br />';
+     }
+     destination.innerHTML = sContents + aText[iIndex].substring(0, iTextPos) + "|";
+     console.log(iTextPos++ == iArrLength);
+     if ( iTextPos++ == iArrLength ) {
+      iTextPos = 0;
+      iIndex++;
+      if ( iIndex != aText.length ) {
+       iArrLength = aText[iIndex].length;
+       setTimeout("typewriter()", 500);
+      }
+     } else {
+      setTimeout("typewriter()", iSpeed);
+     }
+    }
+    
+    
+    typewriter();
